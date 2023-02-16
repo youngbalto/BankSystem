4037102110e1b354896fb82b8f06d8f612a8de59
Andjic Djordje <djordje.andjic99@outlook.com>
2023-02-16 20:20:28 +0100

    Initial commit

---
+++ app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from models import *
+# Register your models here.
+admin.site.register(Credit)
+admin.site.register(AccountNumber)
+admin.site.register(Creditor)
+admin.site.register(Admin)
+admin.site.register(Client)
+admin.site.register(Director)
+admin.site.register(BookManager)
