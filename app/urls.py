e06d864d1ad52e0a6a3873d54b4c8d762294bbc0
Andjic Djordje <djordje.andjic99@outlook.com>
2023-02-16 21:17:51 +0100

    Hotfix to connect to postgresql database

--- app/urls.py
+++ app/urls.py
@@ -2,16 +2,5 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', index),
-    path('login/user',client_login),
-    path('login/accountant',accountant_login),
-    path('sign_up/user',create_new_user),
-    path('sign_up/acocuntant',create_new_accountant),
-    path('login/director',direcotr_login),
-    path('login/credittor',credittor_login),
-    path('sign_up/credittor',create_new_credittor),
-    path('credits/',show_all_credits),
-    path('credits/<id>',show_credit_details),
-    path('profile/director',show_director_details),
-    path('client/<id>',client_details)
+    path('', index)
 ]
--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'djangoProject1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BankSystem',
+        'USER': 'postgres',
+        'PASSWORD': 'user',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
