959f7bd96d61ba3f475f17f92f252003038021aa
Andjic Djordje <djordje.andjic99@outlook.com>
2023-02-18 22:50:09 +0100

    ADD models to the database with the connections

--- app/models.py
+++ app/models.py
@@ -1,34 +1,34 @@
 from django.db import models
-from datetime import date
+from django.utils.timezone import *
 
 # Create your models here.
 # TODO write model fields and relationships
 class AccountNumber(models.Model):
     number = models.CharField(max_length=20, null=False, primary_key=True)
-    entry_date = models.DateField(default=date.today())
+    entry_date = models.DateField(default=now())
     account_balance = models.IntegerField(default=0, null=False)
 
 
+class Creditor(models.Model):
+    name = models.CharField(null=False,max_length=20)
+    account = models.OneToOneField(AccountNumber,models.CASCADE)
+
 class Credit(models.Model):
-    sum = models.IntegerField(null=False,max_length=8)
+    sum = models.IntegerField(null=False)
     date_signed = models.DateField()
     date_due = models.DateField()
-
-
-class Creditor(models.Model):
-    name = models.CharField(null=False)
-    account = models.OneToOneField(AccountNumber,models.CASCADE)
-    credit = models.OneToManyField(Credit,models.CASCADE)
+    creditor = models.ForeignKey(Creditor,on_delete=models.CASCADE)
 
 
 class Admin(models.Model):
-    username = models.CharField(null=False)
-    password = models.CharField(null=False)
+    username = models.CharField(null=False,max_length=10)
+    password = models.CharField(null=False,max_length=18)
 
 
 class Client(models.Model):
     passportNumber = models.CharField(max_length=15, null=False)
     name = models.CharField(max_length=15, null=False)
+    password = models.CharField(max_length=18,null=False)
     surname = models.CharField(max_length=15, null=False)
     client_number = models.OneToOneField(AccountNumber, on_delete=models.CASCADE, primary_key=True)
 
@@ -42,5 +42,6 @@ class Director(models.Model):
 class BookManager(models.Model):
     name = models.CharField(max_length=30, null=False)
     email = models.CharField(max_length=12, null=False)
+    password = models.CharField(max_length=18,null=False)
     phone = models.CharField(max_length=12, null=False)
     is_main = models.BooleanField(default=False)
